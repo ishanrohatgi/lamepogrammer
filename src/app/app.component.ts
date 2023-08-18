9deb35123c8c6c3f3b07a69c21805a4d8885d2b6
Ishan Rohatgi <ishan.77775@gmail.com>
2023-08-18 13:36:33 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'slds';
+  jokes: any;
+  isLoaded = false;
+  constructor(private http: HttpClient){
+  }
+  ngOnInit(): void {
+    //window.addEventListener('online', this.getJokes);
+      this.getJokes();
+  }
+  getJokes(){
+    this.isLoaded = false;
+    const url = 'https://geek-jokes.sameerkumar.website/api?format=json';
+    this.http.get(url).subscribe(
+      (resp)=>{
+        console.log(resp)
+        this.jokes = resp;
+        this.isLoaded=true;
+      },()=>{
+        this.isLoaded=false;
+        if(!this.isLoaded){
+          this.getJokes();
+        }
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NglModule,  NGL_ICON_CONFIG, NglIconConfig} from 'ng-lightning';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NglModule,
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: NGL_ICON_CONFIG, useValue: <NglIconConfig>{ svgPath: 'assets/icons' } },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
